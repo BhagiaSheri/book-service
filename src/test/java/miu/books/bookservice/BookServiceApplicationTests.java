21f8011a9ac2c410e6782faae1b08e0dae994dfb
BhagiaSheri <bhagiasheri24@gmail.com>
2023-07-09 20:23:58 -0500

    book-service startup code

---
+++ src/test/java/miu/books/bookservice/BookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package miu.books.bookservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
